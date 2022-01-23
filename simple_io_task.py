c34497d6c5b41bd3ab5189b155e8737cda8581b2
Johannes Eder <johannes.eder@icloud.com>
2022-01-23 17:28:54 +0100

    updated code to do 100 downloads of the url - not 10

--- simple_io_task.py
+++ simple_io_task.py
@@ -33,5 +33,5 @@ if __name__ == "__main__":
 	url = [
 		"https://logos-world.net/wp-content/uploads/2021/10/Python-Symbol.png"
 	]
-	# pass an array that contains 10 times the url from above
-	download_pics(url*10)
+	# pass an array that contains 100 times the url from above
+	download_pics(url*100)
